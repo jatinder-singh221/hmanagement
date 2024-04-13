cf456be30b4ee0b61a6838e51c09fd0b2b792958
jatinder-singh221 <jatindersing19@gmail.com>
2024-04-14 01:20:54 +0530

    refactor

---
+++ security/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from .views import LoginCustomView, SignUpView, ProfileView, ChangePasswordView, LogoutView, \
+    Forget
+    
+
+urlpatterns = [
+    path('login/', LoginCustomView.as_view(), name='login'),
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('profile/', ProfileView.as_view(), name='profile'),
+    path('security/', ChangePasswordView.as_view(), name='pass'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+]
